8b2534d2e82175794e0fe623ffe33405fe36c3e4
zy_wan <jt6562@gmail.com>
2016-07-06 02:12:25 -0400

    对pc1的改良

--- pc1.py
+++ pc1.py
@@ -9,6 +9,13 @@ def transla(strings):    #函数实现将英文字母往后推移两位得到新
     apstr=''.join(strlist)  #列表合成为字符串 str.join(list)，str为连接字符
     return apstr
 
+#另一种解法
+#for x in s:
+#   if ord(x)>=ord('a') and ord(x)<=ord('z'):
+#       o+=chr((ord(x)+2-ord('a'))%26+ord('a'))
+#   else:
+#       o+=x
+#return o
 secr=input('请输入密文字符串')
 print('解密后明文为：%s' % transla(secr))
 
