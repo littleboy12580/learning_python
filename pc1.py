f7a4f38b7a7c25b216a31857ea1a39d00478e185
zy_wan <jt6562@gmail.com>
2016-07-06 01:07:42 -0400

    添加了pythonchallenge的第一题实现

---
+++ pc1.py
@@ -0,0 +1,15 @@
+def transla(strings):    #函数实现将英文字母往后推移两位得到新的字符串
+    strlist=list(strings)    #此处将字符串转为list
+    for i in range(len(strlist)):
+        if(strlist[i]!=' ' and strlist[i]!='(' and strlist[i]!=')' and strlist[i]!='.' and strlist[i]!="'"):
+            if(strlist[i]=='y' or strlist[i]=='z'):
+                strlist[i]=chr((ord(strlist[i])-24))
+            else:
+                strlist[i]=chr((ord(strlist[i])+2))
+    apstr=''.join(strlist)  #列表合成为字符串 str.join(list)，str为连接字符
+    return apstr
+
+secr=input('请输入密文字符串')
+print('解密后明文为：%s' % transla(secr))
+
+
