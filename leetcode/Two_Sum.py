5fcec76bb5f0a895af75f92dd9c8e40b296ef9f2
zy_wan <jt6562@gmail.com>
2016-07-11 08:29:15 -0400

    update the code

--- leetcode/Two_Sum.py
+++ leetcode/Two_Sum.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 class Solution(object):
-    def twoSum(self,nums,target):
+    def Sum(self,nums,target):
         for i in range(len(nums)):
             if(target-nums[i]) in nums[(i+1):]:
                 return [i,nums.index(target-nums[i])]
@@ -9,11 +9,11 @@ class Solution(object):
 #上面是我自己的解法，复杂度比较高，耗时56ms
 #看了一些网上的解法发现使用字典的话可以降低复杂度
 #下面是使用字典的解法
-def twoSum(self,nums,target):
+def Sum(self,nums,target):
     dictMap={}
-    li=[]
+    final_list=[]
     for index,value in enumerate(nums):    #python内置的遍历函数，第一个是序号，第二个是值
         if (target - value) in dictMap:           #这种方式只会遍历一遍列表
-            li.append([dictMap[target - value], index]) #我对网上解法的改进，考虑了多解的情况
+            final_list.append([dictMap[target - value], index]) #我对网上解法的改进，考虑了多解的情况
         dictMap[value]=index
-    return li                              #耗时46ms
+    return final_list                              #耗时46ms
