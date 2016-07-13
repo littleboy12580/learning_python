a696cd7ee47164ddc8c731e8e3e5034fcd69b149
zy_wan <jt6562@gmail.com>
2016-07-13 06:39:04 -0400

    update the code

--- data_structure/my_stack.py
+++ data_structure/my_stack.py
@@ -5,18 +5,18 @@ class Stack:
     def __init__(self,size):
         self.stack = []
         self.size = size             #栈的大小
-        self.top = -1                #栈顶对象
+       # self.top = -1                #栈顶对象
 
     def isempty(self):               #判断栈是否为空
         return len(self.stack) == 0
 
     def isfull(self):                #判断栈是否已满
-        return self.top+1 == self.size
+        return self.length() == self.size
 
     def push(self,item):             #入栈
         if not self.isfull():
             self.stack.append(item)
-            self.top+=1
+        #    self.top+=1
         else:
             print("栈已满")
 
@@ -29,15 +29,15 @@ class Stack:
     def length(self):                #获取栈内目前对象数量
         return len(self.stack)
 
-    def changesize(self,num):        #修改栈的大小
+    def change_size(self,num):        #修改栈的大小
         self.size=num
 
-    def getsize(self):               #获取栈的大小
+    def get_size(self):               #获取栈的大小
         return self.size
 
-    def gettop(self):                #获取栈顶对象
+    def get_top(self):                #获取栈顶对象
         if not self.isempty():
-            return self.stack[len(self.stack)-1]
+            return self.stack[self.length()-1]
         else:
             print("栈已空")
 
