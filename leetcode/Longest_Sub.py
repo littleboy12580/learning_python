c7971156c21358e2c6fdb543f2310b21c1cbd08b
zy_wan <jt6562@gmail.com>
2016-07-13 04:39:36 -0400

    add the third question

---
+++ leetcode/Longest_Sub.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution(object):                                    #解法一：使用列表
+    def len_longestsub(self,s):
+        maxlength=0
+        usedlist=[]
+        for i in s:
+            if i in usedlist:                              #判断该字符串是否已经出现过
+                if len(usedlist)>maxlength:
+                    maxlength = len(usedlist)
+                usedlist = usedlist[usedlist.index(i)+1:]  #采用分片构建新字符串
+            usedlist.append(i)
+            maxlength = max(len(usedlist),maxlength)       #计算最大长度
+        return maxlength
+
+class Solution(object):                                    #解法二：使用字典
+    def len_longestsub(self,s):
+        start = maxlength = 0
+        usedChar = {}
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:   #根据字典键对应值会覆盖前面值的特点
+                start = usedChar[s[i]] + 1
+            else:
+                maxlength = max(maxlength,i-start+1)
+            usedChar[s[i]] = i
+        return maxlength
