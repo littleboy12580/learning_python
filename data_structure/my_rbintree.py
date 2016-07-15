b0c1cd55d129b4df9c5d52d2bc1e46d492f7ee21
zy_wan <jt6562@gmail.com>
2016-07-14 22:51:20 -0400

    upload the real bintree

--- data_structure/my_rbintree.py
+++ data_structure/my_rbintree.py
@@ -1,25 +1,25 @@
-class BinTree(object):
+class BinTree(object):                            #创建树的类
     def __init__(self,value):
         self._data = value
         self._left = None
         self._right = None
 
-    def insert(self,value):
+    def insert(self,value):                       #树的节点插入，左子树小于根节点，右子树大于根节点
         if self is None:
             self = BinTree(value)
         else:
-            if value < self._data:
+            if value < self._data:                #左边迭代插入
                 if self._left:
                     self._left.insert(value)
                 else:
                     self._left = BinTree(value)
             else:
-                if self._right:
+                if self._right:                   #右边迭代插入
                     self._right.insert(value)
                 else:
                     self._right = BinTree(value)
 
-    def pre_order(self):
+    def pre_order(self):                          #前序遍历
         if self:
             print(self._data,end=' ')
         if self._left:
@@ -27,31 +27,35 @@ class BinTree(object):
         if self._right:
             self._right.pre_order()
 
-    def in_order(self):
-        if self:
+    def in_order(self):                           #中序遍历
+        if self._left:
             self._left.in_order()
+        if self:
             print(self._data,end=' ')
+        if self._right:
             self._right.in_order()
 
-    def post_order(self):
-        if self:
+    def post_order(self):                         #后序遍历
+        if self._left:
             self._left.post_order()
+        if self._right:
             self._right.post_order()
+        if self:
             print(self._data,end=' ')
 
-    def find_min(self):
+    def find_min(self):                           #获取最小值
         if self._left:
             return self._left.find_min()
         else:
             return self
 
-    def find_max(self):
+    def find_max(self):                           #获取最大值
         if self._right:
             return self._right.find_max()
         else:
             return self
 
-    def find(self,value):
+    def find(self,value):                         #查找树中是否有该值
         if value == self._data:
             return self
         elif value < self._data and self._left:
@@ -61,15 +65,15 @@ class BinTree(object):
         else:
             return None
 
-    def del_value(self,value):
+    def del_value(self,value):                    #删除特定值节点
         if self.find(value):
             if value<self._data:
-                self._left=self._left.del_value(value)
+                self._left=self._left.del_value(value)    #左边迭代删除
                 return self
             elif value>self._data:
-                self._right=self._right.del_value(value)
+                self._right=self._right.del_value(value)  #右边迭代删除
                 return self
-            elif self._left and self._right:
+            elif self._left and self._right:              #如果左右子树都存在，找到右子树里最小的来代替被删除节点
                 tmp=self._right.find_min()._data
                 self._data=tmp
                 self._right=self._right.del_value(tmp)
